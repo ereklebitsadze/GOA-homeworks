3b1dc5b4e72eeddb8646a683ffda43d3e6bf7b78
ereklebitsadze <ereklebitsadze11@gmail.com>
2025-06-06 23:01:47 +0400

    first commit

---
+++ day 0/lesson 0.py
@@ -0,0 +1,76 @@
+from turtle import *
+
+speed(10)
+
+width(7)
+color("orange")
+begin_fill()
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(70)
+end_fill()
+color("brown")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+penup()
+goto(200, 200)
+pendown()
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+#Homework:
+
+#first window
+
+color("blue")
+begin_fill()
+penup()
+goto(50, 70)
+pendown()
+right(60)
+forward(30)
+right(90)
+forward(60)
+right(90)
+forward(30)
+right(90)
+forward(60)
+end_fill()
+
+#second window
+
+color("blue")
+begin_fill()
+penup()
+goto(180, 70)
+pendown()
+right(90)
+forward(30)
+right(90)
+forward(60)
+right(90)
+forward(30)
+right(90)
+forward(60)
+end_fill()
+
+exitonclick()
+
+#House Is Done For You Sir

38fedd212245fbacbed70b5981caa701e6441a31
ereklebitsadze <ereklebitsadze11@gmail.com>
2025-07-03 15:42:03 +0400

    second commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,23 @@
+#1
+print("favourite color = black")
+print("favourite food = french fries")
+print("favourite hobby = mtb(mountain biking)")
+
+#2
+movie1 = "pacific rim"
+movie2 = "breaking bad"
+movie3 = "star wars"
+movie4 = "inseption"
+movie5 = "dexter"
+
+#3
+#კონკატინაცია აკავშირებს ორ ცვლადს
+
+#4
+print("today we have learnt concatenation, print, integer, syntax, string ")
+
+#5
+name = "ერელკე"
+age = 14
+
+print("მე მქვია", name, "მე ვარ", age, "წლის")

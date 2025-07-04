567af941ba48fc1da74a86368596bab12b0f27b4
ereklebitsadze <ereklebitsadze11@gmail.com>
2025-07-05 00:08:06 +0400

    second commit

---
+++ day 2/hw/homework2.py
@@ -0,0 +1,24 @@
+name = "ლუკა"
+birthday = 2015
+print("მოგესალმებით", name,"დაბადების დღე გაქვს", birthday,"წელს")
+
+score = 40/100
+print("you score is", score)
+
+health = "67.24HP"
+print("you healed and ur total health is", health)
+
+family1 = "deda dedisshvili"
+family2 = "mama mamishvili"
+family3 = "da disshvili"
+
+print("მამაჩემია", family2,"დედაჩემია", family1, "ჩემი დაა", family3 )
+
+name = "dav"
+name2 = "it"
+
+#კონკატაციით შევაერთებთ აი ესე
+
+print(name + name2)
+
+#დღეს ვისწავლეთ უფრო სწორი იქნება ჩავუღმავდით კონკატაციას და ასევე ვისწავლეთ debugging ანუ შეცდომების გამოსწორება სკრიპტში

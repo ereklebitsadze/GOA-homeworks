0baad7282eceae31db77e783f32eddf1cc76b2a0
ereklebitsadze <ereklebitsadze11@gmail.com>
2025-07-14 14:43:50 +0400

    second commit

---
+++ day 4/cw/classwork1.py
@@ -0,0 +1,48 @@
+# +
+print(16+1)
+print(15+1)
+print(14+1)
+print(13+1)
+print(12+1)
+
+# -
+print(16-1)
+print(15-1)
+print(14-1)
+print(13-1)
+print(12-1)
+
+# //
+print(16//1)
+print(15//1)
+print(14//1)
+print(13//1)
+print(12//1)
+
+# / 
+print(16/1)
+print(56/1)
+print(13/1)
+print(13/1)
+print(2/1)
+
+# *
+print(13*541)
+print(13*2423)
+print(13*24124)
+print(13*241)
+print(13*2421)
+
+# **
+print(13**41)
+print(13**423)
+print(13**4124)
+print(13**41)
+print(13**421)
+
+# %
+print(13%541)
+print(13%2423)
+print(13%24124)
+print(13%241)
+print(13%2421)
---
+++ day 4/hw/homework.py
@@ -0,0 +1,38 @@
+# მათემათიკური ოპერატორები ასრულებენ მოქმედებას ორ ინტეჯერს ან ფლოათს შორის
+
+number1 = input("")
+number2 = input("")
+
+number1 = int(number1)
+number2 = int(number2)
+
+sum = number1 + number2
+print(sum)
+
+a = 20
+b = 10
+
+b-a
+b+a
+b*a
+b/a
+b%a
+b**a
+
+print(b-a)
+print(b+a)
+print(b*a)
+print(b/a)
+print(b%a)
+print(b**a)
+
+x = 4
+y = 3
+x * y
+print(x * y)
+
+print(22>23)
+print(22<23)
+print(22>=23)
+print(22<=23)
+print(22==23)

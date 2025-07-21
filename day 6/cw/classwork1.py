2cd6156c81df695a458036bb61faa95ca793f6cf
ereklebitsadze <ereklebitsadze11@gmail.com>
2025-07-21 14:52:08 +0400

    second commit

---
+++ day 6/cw/classwork1.py
@@ -0,0 +1,27 @@
+# 1)
+
+print("350" + "350")
+
+# 2)
+
+# string
+print("hello")
+
+# integer
+print(1)
+
+# float
+print(5/2)
+
+# boolean
+print(2>3)
+
+# 3)
+
+s = "1"
+i = int(s)
+print(i)
+
+i = 2
+s = str(i)
+print(s)
---
+++ day 6/hw/homework1.py
@@ -0,0 +1,33 @@
+# Boolean ეს არის მონაცემთა ტიპი რომელსაც მხოლოდ ორი მნიშვნელობა შეუძლია ჰქონდეს: true ან false
+
+print("gg")
+print("ggd")
+print("ggf")
+print("ggg")
+print("ggh")
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+print(1.1)
+print(1.2)
+print(1.3)
+print(1.4)
+print(1.5)
+
+print(1>3)
+print(1<5)
+print(1>1)
+print(1<8)
+print(1<9)
+
+a = input()
+
+b = input()
+
+print(a>b)
+
+# თუ ნათურა ჩართულია პროგრამა გვიჩვენებს True და თუ გამორთულია False

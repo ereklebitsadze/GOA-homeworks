4b0c58f1ecb76ff4a686becd7f18104e817ef90b
ereklebitsadze <ereklebitsadze11@gmail.com>
2025-08-21 20:42:24 +0400

    second commit

---
+++ day 15/hw/homework1.py
@@ -0,0 +1,20 @@
+#1)
+#while loop არის ციკლი რომელიც ასრულებს კოდს მანამ სანამ მოცემული პირობა ჭეშმარიტია
+
+#2)
+num = 4
+while num <= 84:
+    if num != 6:
+        print(num)
+    num += 1
+
+#3)
+my_list = [1, 2, 3, 4, 5, 6]
+print(my_list[1])
+print(my_list[3])
+print(my_list[5])
+
+#4)
+x = 5              # ცვლადი
+my_list = [1, 2, 3]  # ლისტი
+print(my_list[1])  # გამოიტანს მეორე ელემენტს: 2

03e522a608b2512c412519bfeeae97f1ea685004
ereklebitsadze <ereklebitsadze11@gmail.com>
2025-09-16 15:52:49 +0400

    second commit

---
+++ day 21/cw/classwork1.py
@@ -0,0 +1,7 @@
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+size = len(my_list)
+print("Size of the list: " + str(size))
+
+for i in range(len(my_list)):
+    print("Element at index " + str(i) + " is " + str(my_list[i]))
---
+++ day 21/hw/homework1.py
@@ -0,0 +1,37 @@
+#1)
+
+letters = ['ა', 'ბ', 'გ', 'დ', 'ე', 'ვ', 'ზ', 'თ', 'ი', 'კ']
+
+first_five = letters[:5]
+last_three = letters[-3:]
+reversed_list = letters[::-1]
+second_to_sixth = letters[1:6]
+
+print(first_five)
+print(last_three)
+print(reversed_list)
+print(second_to_sixth)
+
+
+
+#2)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+every_second = numbers[::2]
+last_four_reversed = numbers[-4:][::-1]
+
+print(every_second)
+print(last_four_reversed)
+
+
+
+#3)
+
+text = "დილამშვიდობისა"
+
+dila = text[:4]
+mshvidobisa = text[4:]
+
+print(dila)
+print(mshvidobisa)

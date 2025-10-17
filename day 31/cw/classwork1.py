a73066ee393775cf10c5fa99db111a336e9ad587
ereklebitsadze <ereklebitsadze11@gmail.com>
2025-10-17 21:18:47 +0400

    second commit

---
+++ day 31/cw/classwork1.py
@@ -0,0 +1,12 @@
+def skeleton(a, b):
+    if a + b == 55:
+        return "Here u go 55 bone"
+    else:
+        return "U never get bones!"
+
+
+a = int(input("1 ricxvi: "))
+b = int(input("2 ricxvi: "))
+
+print("You entered:", a, "and", b)
+print(skeleton(a, b))
